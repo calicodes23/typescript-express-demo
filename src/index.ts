3b3a14e5e9a6ba8e7790fcc10ed6c65132349f7c
Cali Tabilas <ctabilas23@gmail.com>
2020-03-31 21:42:51 -0400

    docker compose working

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,13 @@ app.get("/", (req, res) => {
   res.send(`John is super handsomeeee`);
 });
 
+app.get("/port", (req, res) => {
+  res.send(`LOG: this test is the process.env ${process.env.PORT}`);
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`she's listening on PORT: ${PORT}`);
+  console.log(`CHANGE listening on PORT: ${PORT}
+  `);
 });
